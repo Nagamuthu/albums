4368c917fc0f95511eb7bd80b95a79a03afd974f
Nagamuthu <nagamuthu@hcl.com>
2017-05-09 23:39:19 +0530

    my first react-native project

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+//import library for creating components
+import React from 'react';
+import { Text } from 'react-native';
+
+//create components
+const Header = () => <Text>My Albums</Text>;
+
+//make the components availabel to other page
+export default Header;
